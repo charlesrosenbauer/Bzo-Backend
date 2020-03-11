b9f11cd3705d309712e8bc7dd54248d6c92d1df9
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2020-03-10 22:54:45 -0500

    Code for generating REG-REG x86 instructions.

--- codegen.c
+++ codegen.c
@@ -2,6 +2,17 @@
 #include "util.h"
 
 
+uint8_t x86Opcodes(OPCODE op){
+  return 0;
+}
+
+
+
+
+
+
+
+
 // Functions for writing immediates and displacements
 
 int writeU8 (uint8_t* code, uint64_t imm, int ix){
@@ -54,5 +65,37 @@ int insertInstruction(CODEBUFFER* buffer, MACHINEINSTRUCTION instruction){
 
 
 
+  return 0;
+}
+
+
+
+
+int instructionRegReg(CODEBUFFER* buffer, REGREGINST op){
+  if(op.size != DWORD){
+    // 8 / 16 / 32 bit
+    uint8_t mod = 0xC0 | ((op.a & 0x7) << 3) | (op.b & 0x7);
+    uint8_t opw = (op.size != BYTE)? 1 : 0;
+    uint8_t opd = 0;
+    if(op.size == HALF){
+      buffer->bytes[buffer->length] = 0x66;
+      buffer->length++;
+    }
+    uint8_t opc = x86Opcodes(op.op) | opd | opw;
+    buffer->bytes[buffer->length  ] = opc;
+    buffer->bytes[buffer->length+1] = mod;
+    buffer->length += 2;
+  }else{
+    // 64 bit
+    uint8_t mod = 0xC0 | ((op.a & 0x7) << 3) | (op.b & 0x7);
+    uint8_t opw = (op.size != BYTE)? 1 : 0;
+    uint8_t opd = 0;
+    uint8_t pfx = 0x48 | ((op.a >> 1) & 8) | ((op.b >> 3) & 1);
+    uint8_t opc = x86Opcodes(op.op) | opd | opw;
+    buffer->bytes[buffer->length  ] = pfx;
+    buffer->bytes[buffer->length+1] = opc;
+    buffer->bytes[buffer->length+2] = mod;
+    buffer->length += 3;
+  }
   return 0;
 }
--- codegen.h
+++ codegen.h
@@ -16,25 +16,51 @@ typedef enum{
 }OPSIZE;
 
 typedef enum{
-  RAX = 0,
-  RBX = 3,
-  RCX = 1,
-  RDX = 2,
-  RSP = 4,
-  RBP = 5,
-  RSI = 6,
-  RDI = 7
-}REGISTERS;
+  RAX =  0,
+  RBX =  3,
+  RCX =  1,
+  RDX =  2,
+  RSP =  4,
+  RBP =  5,
+  RSI =  6,
+  RDI =  7,
+  R8  =  8,
+  R9  =  9,
+  R10 = 10,
+  R11 = 11,
+  R12 = 12,
+  R13 = 13,
+  R14 = 14,
+  R15 = 15
+}REGISTER;
 
 
 typedef struct{
   OPCODE op;
-  int    a, b, c;
+  REGISTER  a, b, c;
   uint8_t   prefixes[4];
   OPSIZE    size;
   uint32_t  disp;
   uint64_t  imm;
 }MACHINEINSTRUCTION;
+
+
+typedef struct{
+  OPCODE    op;
+  REGISTER  a, b;
+  OPSIZE    size;
+  uint64_t  imm;
+  int8_t    hasImm;
+}REGREGINST;
+
+typedef struct{
+  OPCODE    op;
+  REGISTER  x;
+  OPSIZE    size;
+  int       offset;
+  uint64_t  imm;
+  int8_t    hasImm;
+}REGSTACKINST;
 
 
 typedef struct{
