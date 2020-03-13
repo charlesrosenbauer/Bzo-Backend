2a26ebfe3fcb075d36396c8240119879296fcc8b
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2020-03-13 01:13:42 -0500

    Moved some codegen stuff to x86 files

--- codegen.h
+++ codegen.h
@@ -11,62 +11,10 @@
 
 
 
-typedef enum{
-  BYTE, HALF, WORD, DWORD, V128, V256
-}OPSIZE;
-
-typedef enum{
-  RAX =  0,
-  RBX =  3,
-  RCX =  1,
-  RDX =  2,
-  RSP =  4,
-  RBP =  5,
-  RSI =  6,
-  RDI =  7,
-  R8  =  8,
-  R9  =  9,
-  R10 = 10,
-  R11 = 11,
-  R12 = 12,
-  R13 = 13,
-  R14 = 14,
-  R15 = 15
-}REGISTER;
 
 
-typedef struct{
-  OPCODE op;
-  REGISTER  a, b, c;
-  uint8_t   prefixes[4];
-  OPSIZE    size;
-  uint32_t  disp;
-  uint64_t  imm;
-}MACHINEINSTRUCTION;
 
 
-typedef struct{
-  OPCODE    op;
-  REGISTER  a, b;
-  OPSIZE    size;
-  uint64_t  imm;
-  int8_t    hasImm;
-}REGREGINST;
-
-typedef struct{
-  OPCODE    op;
-  REGISTER  x;
-  OPSIZE    size;
-  int       offset;
-  uint64_t  imm;
-  int8_t    hasImm;
-}REGSTACKINST;
-
-
-typedef struct{
-  MACHINEINSTRUCTION*  instructions;
-  int                  opcount, ins, exs;
-}CODEBLOCK;
 
 
 typedef struct{
@@ -76,11 +24,6 @@ typedef struct{
 
 
 
-
-
-
-
-int insertInstruction(CODEBUFFER*, MACHINEINSTRUCTION);
 
 
 
---
+++ x86.h
@@ -0,0 +1,75 @@
+#ifndef __X86_HEADER__
+#define __X86_HEADER__
+
+
+
+#include "stdint.h"
+#include "util.h"
+#include "codegen.h"
+
+
+
+
+typedef enum{
+  BYTE, HALF, WORD, DWORD, V128, V256
+}OPSIZE;
+
+typedef enum{
+  RAX =  0,
+  RBX =  3,
+  RCX =  1,
+  RDX =  2,
+  RSP =  4,
+  RBP =  5,
+  RSI =  6,
+  RDI =  7,
+  R8  =  8,
+  R9  =  9,
+  R10 = 10,
+  R11 = 11,
+  R12 = 12,
+  R13 = 13,
+  R14 = 14,
+  R15 = 15
+}REGISTER;
+
+
+typedef struct{
+  OPCODE op;
+  REGISTER  a, b, c;
+  uint8_t   prefixes[4];
+  OPSIZE    size;
+  uint32_t  disp;
+  uint64_t  imm;
+}MACHINEINSTRUCTION;
+
+
+typedef struct{
+  OPCODE    op;
+  REGISTER  a, b;
+  OPSIZE    size;
+  uint64_t  imm;
+  int8_t    hasImm;
+}REGREGINST;
+
+typedef struct{
+  OPCODE    op;
+  REGISTER  x;
+  OPSIZE    size;
+  int       offset;
+  uint64_t  imm;
+  int8_t    hasImm;
+}REGSTACKINST;
+
+typedef struct{
+  MACHINEINSTRUCTION*  instructions;
+  int                  opcount, ins, exs;
+}CODEBLOCK;
+
+
+
+int insertInstruction(CODEBUFFER*, MACHINEINSTRUCTION);
+
+
+
+#endif
