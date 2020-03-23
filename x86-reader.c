04acdc2d0024a93dfbecb027dcaef99cf4515c97
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2020-03-22 23:50:50 -0500

    Added some structs for an x86 opcode table.

---
+++ x86-reader.c
@@ -0,0 +1,13 @@
+#include "parser.h"
+#include "x86-reader.h"
+#include "stdint.h"
+#include "stdlib.h"
+
+
+
+
+
+
+
+
+
---
+++ x86-reader.h
@@ -0,0 +1,52 @@
+#ifndef __X86_READER_HEADER__
+#define __X86_READER_HEADER__
+
+
+#include "stdint.h"
+
+
+typedef enum{
+	RAX = 0x00001,
+	RBX = 0x00002,
+	RCX = 0x00004,
+	RDX = 0x00008,
+	RBP = 0x00010,
+	RSP = 0x00020,
+	RSI = 0x00040,
+	RDI = 0x00080,
+	R8  = 0x00100,
+	R9  = 0x00200,
+	R10 = 0x00400,
+	R11 = 0x00800,
+	R12 = 0x01000,
+	R13 = 0x02000,
+	R14 = 0x04000,
+	R15 = 0x08000,
+	STS = 0x10000,
+	RIP = 0x20000
+}X86REG;
+
+
+
+
+typedef struct{
+	uint32_t opcode;
+	uint8_t  opcodesize;
+	uint8_t  requiredprefix;
+	uint8_t  rmm;
+	uint8_t  inct, exct;
+	uint64_t inmasks[4];
+	uint64_t exmasks[4];
+	uint64_t pipes;
+	uint16_t latency;
+}X86_OPCODE;
+
+
+
+
+
+
+
+
+
+#endif
