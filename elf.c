f5fa0968138161e527cf64355d3117cda3d14e47
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2020-03-25 22:47:47 -0500

    Some initial code for generating elf files.

---
+++ elf.c
@@ -0,0 +1,51 @@
+#include "stdio.h"
+#include "stdint.h"
+#include "elf.h"
+#include "codegen.h"
+
+
+
+
+
+
+int makeELF(uint8_t** program, uint8_t* code, int codesize, uint8_t* data, int datasize){
+	int  buffersize = sizeof(ELFHEADER) + sizeof(PROGRAMHEADER) + codesize + datasize;
+	uint8_t* buffer = malloc(buffersize);
+
+	int bufferhead = 0;
+	ELFHEADER* eheader     = (ELFHEADER*)&buffer[bufferhead];
+	eheader->magic         = 0x464c457f;
+	eheader->bitsize       = 2;
+	eheader->endianness    = 1;
+	eheader->elfversion0   = 1;
+	eheader->abi           = 0;
+	eheader->abiversion    = 0;
+	eheader->objfiletype   = 2;
+	eheader->isa           = 0x3e;
+	eheader->elfversion1   = 1;
+	eheader->entrypos      = 0;		// entry
+	eheader->pheadoffset   = 0x40;
+	eheader->sheadoffset   = 0xb0;
+	eheader->flags         = 0;
+	eheader->headersize    = 64;
+	eheader->pheadersize   = 0x20;
+	eheader->pheadcount    = 1;
+	eheader->sheadersize   = 0x28;
+	eheader->sheadcount    = 4;
+	eheader->sheadindex    = 3;
+
+	bufferhead = 0x40;
+	PROGRAMHEADER* pheader = (PROGRAMHEADER*)&buffer[bufferhead];
+	pheader->p_type        = 1;
+	pheader->p_flags       = 5;
+	pheader->p_offset      = 0;
+	pheader->p_vaddr       = 0x8000000;
+	pheader->p_paddr       = 0x8000000;
+	pheader->p_filesz      = 0x90;
+	pheader->p_memsz       = 0x90;
+	pheader->p_align       = 1;
+
+
+
+	return buffersize;
+}
---
+++ elf.h
@@ -0,0 +1,53 @@
+#ifndef __ELF_HEADER__
+#define __ELF_HEADER__
+
+
+#include "stdint.h"
+#include "codegen.h"
+
+
+
+
+typedef struct{
+	uint32_t  magic;
+	uint8_t   bitsize;
+	uint8_t   endianness;
+	uint8_t   elfversion0;
+	uint8_t   abi;
+	uint8_t   abiversion;
+	uint8_t   unusedA[7];
+	uint16_t  objfiletype;
+	uint16_t  isa;
+	uint32_t  elfversion1;
+	uint64_t  entrypos;
+	uint64_t  pheadoffset;
+	uint64_t  sheadoffset;
+	uint32_t  flags;
+	uint16_t  headersize;
+	uint16_t  pheadersize;
+	uint16_t  pheadcount;
+	uint16_t  sheadersize;
+	uint16_t  sheadcount;
+	uint16_t  sheadindex;
+}ELFHEADER;
+
+
+typedef struct{
+	uint32_t  p_type;
+	uint32_t  p_flags;
+	uint64_t  p_offset;
+	uint64_t  p_vaddr;
+	uint64_t  p_paddr;
+	uint64_t  p_filesz;
+	uint64_t  p_memsz;
+	uint64_t  p_align;
+}PROGRAMHEADER;
+
+
+
+
+
+int makeELF(uint8_t**, uint8_t*, int, uint8_t*, int);
+
+
+#endif
