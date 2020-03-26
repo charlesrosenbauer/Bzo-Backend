adbaf733d8388c6ba054160c55a934656ddcaa6e
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2020-03-25 23:47:52 -0500

    Added a struct for section headers

--- elf.h
+++ elf.h
@@ -44,6 +44,20 @@ typedef struct{
 }PROGRAMHEADER;
 
 
+typedef struct{
+	uint32_t  sh_name;
+	uint32_t  sh_type;
+	uint64_t  sh_flags;
+	uint64_t  sh_addr;
+	uint64_t  sh_offset;
+	uint64_t  sh_size;
+	uint32_t  sh_link;
+	uint32_t  sh_info;
+	uint64_t  sh_addralign;
+	uint64_t  sh_entsize;
+}SECTIONHEADER;
+
+
 
 
 
