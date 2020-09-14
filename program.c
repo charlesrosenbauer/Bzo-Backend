65c3f0dc303d5d001a15b5f008360b520b98027b
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2020-09-13 23:21:47 -0500

    A couple functions for manipulating the program data structure.

--- program.c
+++ program.c
@@ -18,11 +18,33 @@ Program   makeProgram    (int blkCap, int fncCap){
 }
 
 
-void      resizeProgram  (Program* p, int blkCap, int fnCap){
+void      resizeProgram  (Program* p, int blkCap, int fniCap){
 
+	int blCap = (blkCap > p->blockCap)? blkCap : p->blockCap;
+	int fnCap = (fniCap > p->fnCap   )? fniCap : p->fnCap;
+
+	if(blCap == blkCap){
+		CodeBlock* blkTmp = p->blocks;
+		p->blocks   = malloc(sizeof(CodeBlock) * blCap);
+		for(int i   = 0; i < p->blockNum; i++) p->blocks[i] = blkTmp[i];
+		free(blkTmp);
+		p->blockCap = blCap;
+	}
+	
+	if(fnCap == fniCap){
+		int* fniTmp = p->fnids;
+		p->fnids    = malloc(sizeof(int) * fnCap);
+		for(int i   = 0; i < p->fnNum; i++) p->fnids[i] = fniTmp[i];
+		free(fniTmp);
+		p->fnCap = fnCap;
+	}
 }
 
 
 void      addProgramBlock(Program* p, CodeBlock blk){
+	if(p->blockCap <= (p->blockNum + 1))
+		resizeProgram(p, p->blockCap * 2, p->fnCap);
 	
+	p->blocks[p->blockNum] = blk;
+	p->blockNum++;
 }
