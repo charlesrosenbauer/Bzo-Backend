7c1ad4beb688970f8dec17d8182c3ffc1b5b4573
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2020-09-14 15:15:19 -0500

    Start of a parseExpr function.

--- ast.h
+++ ast.h
@@ -13,16 +13,21 @@ typedef enum{
 	PE_OKAY,
 	PE_BAD_FN_TYPE,
 	PE_BAD_PATTERN,
-	PE_INCOMPLETE_PATTERN
+	PE_INCOMPLETE_PATTERN,
+	PE_BAD_CMPD,
+	PE_BAD_POLY,
+	PE_BAD_EXPR,
+	PE_BAD_PRFX
 }ParseError;
 
 
 ParseError parseFunction(LISP*, CodeBlock*, int*);
 ParseError parsePattern (LISP*, CodeBlock*, int*);
+ParseError parseExpr    (LISP*, CodeBlock*, int*);
 
 
 
-
+//https://www.youtube.com/watch?v=Hrr3dp7zRQY 24:17
 
 
 
--- pattern.c
+++ pattern.c
@@ -1,2 +1,18 @@
 #include "ast.h"
 
+#include "util.h"
+
+
+
+
+
+ParseErr parseExpr(LISP* l, CodeBlock* blk, int* blkct){
+	VALOBJ kind = lispIx(l, 0);
+	if((kind.typ == OPRTYP) && (kind.val.UVAL == 0x300)){
+		return PE_BAD_EXPR;
+	}
+
+	// Okay, this looks like an expression. Parse it as one.
+
+	return PE_OKAY;
+}
