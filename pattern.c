b0c261cecd54fad110decf254ca993bc4c23c6f0
charlesrosenbauer <charles.rosenbauer@gmail.com>
2020-09-15 23:54:01 -0500

    Frame for parseExpr, Cmpd, and Poly

--- pattern.c
+++ pattern.c
@@ -5,8 +5,32 @@
 
 
 
+ParseError parseCmpd(LISP* l, CodeBlock* blk, int* blkct){
+	VALOBJ kind = lispIx(l, 0);
+	if((kind.typ == OPRTYP) && (kind.val.UVAL == 0x303)){
+		return PE_BAD_EXPR;
+	}
 
-ParseErr parseExpr(LISP* l, CodeBlock* blk, int* blkct){
+	// Okay, this looks like a CMPD. Parse it as one.
+
+	return PE_OKAY;
+}
+
+
+ParseError parsePoly(LISP* l, CodeBlock* blk, int* blkct){
+	VALOBJ kind = lispIx(l, 0);
+	if((kind.typ == OPRTYP) && (kind.val.UVAL == 0x304)){
+		return PE_BAD_EXPR;
+	}
+
+	// Okay, this looks like a POLY. Parse it as one.
+
+	return PE_OKAY;
+}
+
+
+
+ParseError parseExpr(LISP* l, CodeBlock* blk, int* blkct){
 	VALOBJ kind = lispIx(l, 0);
 	if((kind.typ == OPRTYP) && (kind.val.UVAL == 0x300)){
 		return PE_BAD_EXPR;
