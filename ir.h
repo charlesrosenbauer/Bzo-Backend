63ce931996893e1c4f7489a6efa6365da99425f1
charlesrosenbauer <charles.rosenbauer@gmail.com>
2020-11-13 09:11:09 -0600

    Added IR files

---
+++ ir.h
@@ -0,0 +1,18 @@
+#ifndef __IR_HEADER__
+#define __IR_HEADER__
+
+
+#include "util.h"
+
+
+/*
+	TODO Here:
+	
+	We need to be able to apply various transforms to IR representations of code to make it more useable.
+	
+	Types need to be transformed into trees of type structs, though functions can probably just be converted straight to bytecode.
+*/
+
+
+
+#endif
