3f7a207f9cb6cd2c7bc1b3015ab4cd0b291e0101
charlesrosenbauer <charles.rosenbauer@gmail.com>
2021-01-20 13:59:59 -0600

    Handling modr/m and prefixes a little better in x86 codegen

--- simple-backend/x86.c
+++ simple-backend/x86.c
@@ -3,29 +3,60 @@
 #include "codegen.h"
 
 
-uint8_t modrm(X86Op opc){
-	return 0;
+uint16_t makeRRModrm(X86Op opc){
+	if((opc.ra >= XMM0) && (opc.ra <= NOREG)) return 0;
+	if((opc.rb >= XMM0) && (opc.ra <= NOREG)) return 0;
+	
+	uint8_t a = opc.ra;
+	uint8_t b = opc.rb;
+	
+	uint16_t ret = 0;
+	ret |= (a & 8) << 1;
+	ret |= (b & 8) << 3;
+	
+	ret |= 0xC0;
+	ret |= (a & 0x7);
+	ret |= (b & 0x7) << 3;
+
+	return ret;
 }
 
 
-int simpleOpcode(X86Size sz, uint8_t opcode, uint8_t* bytes, int head){
+int simpleOpcode(X86Size sz, uint8_t opcode, uint16_t modrm, uint8_t* bytes, int head){
+
+	uint8_t prefix = 0;
+	if((modrm & 0xff00) && (sz != SC_64))
+		prefix = 0x40 | (modrm >> 8);
+
 	if(sz == SC_64){
-		bytes[head  ] = 0x48;
+		bytes[head  ] = 0x48 | (modrm >> 8);
 		bytes[head+1] = opcode | 1;
-		//bytes[head+2] = modrm(opc);
+		bytes[head+2] = modrm & 0xff;
 		return 3;
 	}else if(sz == SC_32){
+		if(prefix){
+			bytes[head] = prefix;
+			head++;
+		}
 		bytes[head  ] = opcode | 1;
-		//bytes[head+1] = modrm(opc);
+		bytes[head+1] = modrm & 0xff;
 		return 2;
 	}else if(sz == SC_16){
 		bytes[head  ] = 0x66;
+		if(prefix){
+			bytes[head+1] = prefix;
+			head++;
+		}
 		bytes[head+1] = opcode | 1;
-		//bytes[head+2] = modrm(opc);
+		bytes[head+2] = modrm & 0xff;
 		return 3;
 	}else if(sz == SC_8){
+		if(prefix){
+			bytes[head] = prefix;
+			head++;
+		}
 		bytes[head  ] = opcode;
-		//bytes[head+1] = modrm(opc);
+		bytes[head+1] = modrm & 0xff;
 		return 2;
 	}
 	return 0;
@@ -35,41 +66,41 @@ int simpleOpcode(X86Size sz, uint8_t opcode, uint8_t* bytes, int head){
 int writeX86(X86Op opc, uint8_t* bytes, int head){
 	switch(opc.opc){
 		case XO_ADD : {
-			return simpleOpcode(opc.size,  0, bytes, head);
+			return simpleOpcode(opc.size,  0, makeRRModrm(opc), bytes, head);
 		}break;
 		
 		case XO_SUB : {
-			return simpleOpcode(opc.size, 28, bytes, head);
+			return simpleOpcode(opc.size, 28, makeRRModrm(opc), bytes, head);
 		}break;
 		
 		case XO_ADC : {
-			return simpleOpcode(opc.size, 10, bytes, head);
+			return simpleOpcode(opc.size, 10, makeRRModrm(opc), bytes, head);
 		}break;
 		
 		case XO_SBB : {
-			return simpleOpcode(opc.size, 18, bytes, head);
+			return simpleOpcode(opc.size, 18, makeRRModrm(opc), bytes, head);
 		}break;
 		
 		
 		
 		case XO_AND : {
-			return simpleOpcode(opc.size, 20, bytes, head);
+			return simpleOpcode(opc.size, 20, makeRRModrm(opc), bytes, head);
 		}break;
 		
 		case XO_OR  : {
-			return simpleOpcode(opc.size,  8, bytes, head);
+			return simpleOpcode(opc.size,  8, makeRRModrm(opc), bytes, head);
 		}break;
 		
 		case XO_XOR : {
-			return simpleOpcode(opc.size, 30, bytes, head);
+			return simpleOpcode(opc.size, 30, makeRRModrm(opc), bytes, head);
 		}break;
 		
 		case XO_CMP : {
-			return simpleOpcode(opc.size, 38, bytes, head);
+			return simpleOpcode(opc.size, 38, makeRRModrm(opc), bytes, head);
 		}break;
 		
 		case XO_MOV : {
-			return simpleOpcode(opc.size, 88, bytes, head);
+			return simpleOpcode(opc.size, 88, makeRRModrm(opc), bytes, head);
 		}break;
 		
 		case XO_RET : {
