2ab82d98c33fa7128a41dcb2dbf59930a165bd6f
charlesrosenbauer <charles.rosenbauer@gmail.com>
2021-03-05 15:43:40 -0600

    Outline of parser

--- src/langparser.c
+++ src/langparser.c
@@ -8,7 +8,43 @@
 
 
 
+int parseTypeDef(LexerState* tks, SymbolTable* tab, ASTProgram* prog, int tix, int pix){
+	
+	return 0;
+}
 
-int parseCode(LexerState* tks){
+
+int parseFuncDef(LexerState* tks, SymbolTable* tab, ASTProgram* prog, int tix, int pix){
+	
+	return 0;
+}
+
+
+int parseCode(LexerState* tks, SymbolTable* tab, ASTProgram* prog){
+	int tix = 0;
+	int pix = 0;
+	while(tix < tks->tkct){
+		int skip;
+		if(tks->tks[tix].type == TKN_NEWLINE){
+			tix ++;
+			continue;
+		}
+		
+		skip = parseFuncDef(tks, tab, prog, tix, pix);
+		if(skip > 1){
+			tix += skip;
+			continue;
+		}
+		
+		skip = parseTypeDef(tks, tab, prog, tix, pix);
+		if(skip > 1){
+			tix += skip;
+			continue;
+		}
+		
+		printf("Could not parse file...\n");
+		return -1;
+	}
+
 	return 0;
 }
