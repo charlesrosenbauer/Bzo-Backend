5b35262fb2ae0c15d96be26b6b05881f259bcf53
charlesrosenbauer <charles.rosenbauer@gmail.com>
2021-03-17 01:09:28 -0500

    Better error messages

--- src/error.c
+++ src/error.c
@@ -5,16 +5,37 @@
 #include "error.h"
 
 
+void fmtPosition(Position p, char* buffer){
+	sprintf(buffer, "%i@(%i:%i)", p.fileId, p.lineStart, p.colStart);
+}
+
 
 void fmtError(Error e, char* buffer){
-	switch(e.msg){
-		case ERR_NO_ERR       : return;
-		case ERR_LEXER_FAIL   : {
-			sprintf(buffer, "At %i:%i:%i, lexer failed.\n" , e.pos.fileId, e.pos.lineStart, e.pos.colStart);
-		}break;
-		case ERR_PARSER_FAIL  : {
-			sprintf(buffer, "At %i:%i:%i, parser failed.\n", e.pos.fileId, e.pos.lineStart, e.pos.colStart);
-		}break;
+	char posBuffer[256];
+	fmtPosition(e.pos, posBuffer);
+	if       (e.msg < ERR_X_MAX_X_ERR){
+		sprintf(buffer, "%s Unknown Error\n", posBuffer);
+	}else if (e.msg < ERR_P_MAX_P_ERR){
+		switch(e.msg){
+			case ERR_P_BAD_PAR  : sprintf(buffer, "%s Broken Parentheses.   \n", posBuffer); break;
+			case ERR_P_BAD_BRK  : sprintf(buffer, "%s Broken Bracket.       \n", posBuffer); break;
+			case ERR_P_BAD_BRC  : sprintf(buffer, "%s Broken Curly Brace.   \n", posBuffer); break;
+			case ERR_P_DNGL_PAR : sprintf(buffer, "%s Unmatched Parentheses.\n", posBuffer); break;
+			case ERR_P_DNGL_BRK : sprintf(buffer, "%s Unmatched Bracket.    \n", posBuffer); break;
+			case ERR_P_DNGL_BRC : sprintf(buffer, "%s Unmatched Curly Brace.\n", posBuffer); break;
+			default: sprintf(buffer, "%s Unknown Parser Error\n", posBuffer); break;
+		}
+	}else{
+		sprintf(buffer, "%s Unknown Compiler Error\n", posBuffer);
+	}
+}
+
+void printErrors(ErrorList* errs){
+	// Ideally, we should sort errors first
+	for(int i = 0; i < errs->erct; i++){
+		char buffer[1024];
+		fmtError(errs->errs[i], buffer);
+		printf("%s\n\n", buffer);
 	}
 }
 
--- src/error.h
+++ src/error.h
@@ -3,14 +3,23 @@
 
 
 typedef enum{
-	ERR_NO_ERR		= 0,
-	ERR_LEXER_FAIL  = 1,
-	ERR_PARSER_FAIL = 2,
+	// Control Errors
+	ERR_X_NO_ERR	 = 0x000,
+	
+	ERR_X_MAX_X_ERR  = 0x0FF,
+	
+	// Lexer Errors
+	ERR_L_MAX_L_ERR  = 0x1FF,
 	
 	// Parser Errors
-	ERR_P_BAD_PAR   = 0x100,
-	ERR_P_BAD_BRK   = 0x101,
-	ERR_P_BAD_BRC   = 0x102
+	ERR_P_BAD_PAR    = 0x200,
+	ERR_P_BAD_BRK    = 0x201,
+	ERR_P_BAD_BRC    = 0x202,
+	ERR_P_DNGL_PAR   = 0x203,
+	ERR_P_DNGL_BRK   = 0x204,
+	ERR_P_DNGL_BRC   = 0x205,
+	
+	ERR_P_MAX_P_ERR  = 0x2FF
 }ErrorMsg;
 
 
@@ -29,7 +38,7 @@ typedef struct{
 
 ErrorList makeErrorList(int);
 void      appendError  (ErrorList*, Error);
-
+void      printErrors  (ErrorList*);
 
 
 
