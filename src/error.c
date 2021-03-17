fd97d6d756b65e1733ca6aee08716f445906c145
charlesrosenbauer <charles.rosenbauer@gmail.com>
2021-03-17 00:44:44 -0500

    More errors

--- src/error.c
+++ src/error.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdlib.h"
 
 #include "util.h"
 #include "error.h"
@@ -15,4 +16,28 @@ void fmtError(Error e, char* buffer){
 			sprintf(buffer, "At %i:%i:%i, parser failed.\n", e.pos.fileId, e.pos.lineStart, e.pos.colStart);
 		}break;
 	}
+}
+
+
+
+
+ErrorList makeErrorList(int sz){
+	ErrorList ret;
+	ret.errs  = malloc(sizeof(Error) * sz);
+	ret.erct  = 0;
+	ret.ercap = sz;
+	return ret;
+}
+
+
+void      appendError  (ErrorList* errs, Error e){
+	if(errs->erct+1 >= errs->ercap){
+		Error* tmp = errs->errs;
+		errs->errs = malloc(sizeof(Error) * errs->ercap * 2);
+		for(int i = 0; i < errs->ercap; i++) errs->errs[i] = tmp[i];
+		errs->ercap *= 2;
+		free(tmp);
+	}
+	errs->errs[errs->erct] = e;
+	errs->erct++;
 }
--- src/error.h
+++ src/error.h
@@ -5,7 +5,12 @@
 typedef enum{
 	ERR_NO_ERR		= 0,
 	ERR_LEXER_FAIL  = 1,
-	ERR_PARSER_FAIL = 2
+	ERR_PARSER_FAIL = 2,
+	
+	// Parser Errors
+	ERR_P_BAD_PAR   = 0x100,
+	ERR_P_BAD_BRK   = 0x101,
+	ERR_P_BAD_BRC   = 0x102
 }ErrorMsg;
 
 
@@ -16,10 +21,14 @@ typedef struct{
 }Error;
 
 
+typedef struct{
+	Error*     errs;
+	int        erct, ercap;
+}ErrorList;
 
 
-
-
+ErrorList makeErrorList(int);
+void      appendError  (ErrorList*, Error);
 
 
 
