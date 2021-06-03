a2ef9085e6db6ce7d98b00ec53a381a9681d95db
Charles <charles.rosenbauer@gmail.com>
2021-06-03 02:25:07 -0500

    Created files for yet another parser rewrite. There are obvious ways to simplify the parser by a lot

---
+++ src/ast.h
@@ -0,0 +1,38 @@
+#ifndef __AST_HEADER__
+#define __AST_HEADER__
+
+
+#include "program.h"
+
+
+typedef struct{
+	
+}AST;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ src/bzoparser.c
@@ -0,0 +1,13 @@
+#include "stdint.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+#include "ast.h"
+#include "program.h"
+
+
+
+
+int parseIter(){
+	return 0;
+}
