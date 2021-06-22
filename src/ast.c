45bd53ea191eb2abc81ceb52bcafae8cbd1b1503
Charles <charles.rosenbauer@gmail.com>
2021-06-21 23:58:43 -0500

    Start of some expr printing

--- src/ast.c
+++ src/ast.c
@@ -3,6 +3,20 @@
 
 #include "program.h"
 #include "util.h"
+#include "ast.h"
 
 
 
+
+
+void printASTExpr(ASTExpr* expr){
+	switch(expr->type){
+		case XT_INT : printf(" I%li " , expr->tk.data.i64     ); break;
+		case XT_FLT : printf(" F%f "  , expr->tk.data.f64     ); break;
+		case XT_STR : printf(" S%s "  , expr->tk.data.str.text); break;
+		case XT_TAG : printf(" T%s "  , expr->tk.data.str.text); break;
+		case XT_ID  : printf(" ID%li ", expr->tk.data.i64     ); break;
+		case XT_MID : printf(" MI%li ", expr->tk.data.i64     ); break;
+		default: printf(" (?) "); break;
+	}
+}
