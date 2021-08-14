98d20f990598f71b9b7ff5cbc57b78dd5ffab9b1
Charles <charles.rosenbauer@gmail.com>
2021-08-13 23:55:46 -0500

    Stripped out some old Lisp manipulation code that was still there?

--- src/util.c
+++ src/util.c
@@ -44,68 +44,6 @@ int loadFile(char* fname, uint8_t** buffer, int* fsize){
 
 
 
-void printLisp(Lisp* l){
-  Lisp* here = l;
-  if(here == NULL){
-    printf("nil ");
-    return;
-  }
-
-  printf("(");
-  while(here != NULL){
-    switch(here->here.typ){
-      case FNCTYP : printf("f%lu ", here->here.val.UVAL     ); break;
-      case INTTYP : printf("i%li ", here->here.val.IVAL     ); break;
-      case UNTTYP : printf("u%lu ", here->here.val.UVAL     ); break;
-      case FLTTYP : printf("r%f ",  here->here.val.FVAL     ); break;
-      case STRTYP : printf("s%i ",  here->here.val.SVAL.size); break;
-      case LSPTYP : printLisp(here->here.val.PVAL           ); break;
-      case VARTYP : printf("v%lu ", here->here.val.UVAL     ); break;
-      case OPRTYP : printf("x%lu ", here->here.val.UVAL     ); break;
-      case TYPTYP : printf("t%lu ", here->here.val.UVAL     ); break;
-      case HOLTYP : printf("HOLE"                           ); break;
-      case BSTTYP : {
-        printf("b");
-        for(int i = (64 - __builtin_clzl(here->here.val.BVAL))-1; i >= 0; i--)
-          printf((here->here.val.BVAL & (1l << i))? "#" : "_");
-        printf(" ");
-      }break;
-      default:      printf("%i "  , here->here.typ          ); break;
-    }
-    here = here->next;
-  }
-  printf(") ");
-}
-
-
-
-Valobj lispIx(Lisp* l, int ix){
-  if(l == NULL){
-    printf("Unexpected nil list.\n");
-    exit(9);
-  }
-  for(int i = 1; i <= ix; i++){
-    l = l->next;
-    if(l == NULL){
-      printf("Tried to access element %i of list, only got to element %i.\n", ix, i-1);
-      exit(10);
-    }
-  }
-  return l->here;
-}
-
-
-
-int lispSize(Lisp* l){
-  int size = 0;
-  Lisp* here = l;
-  while(here != NULL){
-    size++;
-    here = here->next;
-  }
-  return size;
-}
-
 
 
 void printHexBuffer(uint8_t* buffer, int size){
